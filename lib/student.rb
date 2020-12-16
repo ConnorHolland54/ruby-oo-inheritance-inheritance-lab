fdcc45dd5ff20725d5cf29c11e501d59aba6c595
Connor Holland <supersoccergoalie123@gmail.com>
2020-12-16 11:50:50 -0700

    commit tree f0a9834da2142f4f272ff955a43a74f9ba31977f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+   attr_accessor :first_name, :last_name, :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def learn(x)
+        @knowledge << x
+    end
 
 end
